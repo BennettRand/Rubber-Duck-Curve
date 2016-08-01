9eec56190c666cbc58308c6193964eb69ea60d65
Ben Rand <brand@solarcity.com>
2016-08-01 16:46:26 -0700

    Initial Modelling Framework

--- rubberduckcurve/__main__.py
+++ rubberduckcurve/__main__.py
@@ -1,4 +1,16 @@
+import datetime
+
+from . import model
+
 def main():
+	p = model.ProductionModel(100, 400)
+	c = model.ConsumptionModel(200, 240)
+
+	for x in xrange(24):
+		curr_t = datetime.time(x)
+		print c.get_power(curr_t), c.get_net_power(curr_t),
+		print p.get_power(curr_t), p.get_net_power(curr_t)
+
 	return
 
 if __name__ == "__main__":
---
+++ rubberduckcurve/model.py
@@ -0,0 +1,44 @@
+import datetime
+
+class Direction(object):
+	Production = -1
+	Consumption = 1
+
+class Model(object):
+	def __init__(self, k_watt_peak, nominal_voltage, direction):
+		self.direction = float(direction)
+		self.k_watt_peak = float(k_watt_peak)
+		self.nominal_voltage = float(nominal_voltage)
+
+	@staticmethod
+	def time_to_minutes(time_):
+		mins = (time_.hour * 60.0) + time_.minute + (time_.second / 60.0)
+		mins += time_.microsecond / (1000000.0 * 60.0)
+		return mins
+
+	def _get_power_normalized(self, time_):
+		return 0.5
+
+	def get_power(self, time_):
+		return self._get_power_normalized(time_) * self.k_watt_peak
+
+	def get_net_power(self, time_):
+		return self.get_power(time_) * self.direction
+
+class ConsumptionModel(Model):
+	def __init__(self, k_watt_peak, nominal_voltage):
+		super(ConsumptionModel, self).__init__(k_watt_peak, nominal_voltage,
+			Direction.Consumption)
+
+	def _get_power_normalized(self, time_):
+		mins = self.time_to_minutes(time_)
+		return (1.0 + min(5.0 * mins, 1440.0)) / max(2.0, 2.0*mins)
+
+class ProductionModel(Model):
+	def __init__(self, k_watt_peak, nominal_voltage):
+		super(ProductionModel, self).__init__(k_watt_peak, nominal_voltage,
+			Direction.Production)
+
+	def _get_power_normalized(self, time_):
+		mins = self.time_to_minutes(time_)
+		return (1.0 / 1440.0) * min(1440.0 - mins, mins)
